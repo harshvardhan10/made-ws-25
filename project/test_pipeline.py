cdc2bb51e4b1af46271b1465a57be1952a8479c4
Harshvardhan Agarwal <agarhars10@gmail.com>
2024-12-11 14:24:25 +0530

    -> fixing path for Github actions. Path configuration that works locally does not work on repo

--- project/test_pipeline.py
+++ project/test_pipeline.py
@@ -6,13 +6,13 @@ import subprocess
 class TestDataPipeline(unittest.TestCase):
     def setUp(self):
         # Ensure the output directory exists
-        self.output_dir = os.path.abspath('../made-ws-25/data')
+        self.output_dir = os.path.abspath('./data')
         if not os.path.exists(self.output_dir):
             os.makedirs(self.output_dir)
 
     def test_data_pipeline(self):
         # Run the data pipeline script
-        script_path = os.path.abspath('../made-ws-25/project/pipeline.sh')
+        script_path = os.path.abspath('./project/pipeline.sh')
         result = subprocess.run(['bash', script_path], capture_output=True, text=True)
         self.assertEqual(result.returncode, 0, msg=f"Pipeline script failed with error: {result.stderr}")
 
